53ac14ab231df9dc16bbfcbd5a6d44221061ebf8
Mattia Paterna <mattia.paterna@gmail.com>
2020-06-19 22:53:44 +0200

    introducing ExceptT

--- haskell/fourth.hs
+++ haskell/fourth.hs
@@ -3,6 +3,8 @@
     (https://youtu.be/YCUYMNEfjb8)
 -}
 
+import Control.Monad.Trans.Except
+
 
 -- Monad Transformers
 --
@@ -19,14 +21,36 @@ dbProgram = dbLookup1 >>= \x ->
 
 -- using do notation, equivalent to above
 dbProgram' = do
-    x   <- dbLookup1                                            -- x :: IO (Either String Int)
-    y   <- dbLookup2                                            -- y :: IO (Either String Int)
-    return $ addEitherValues x y                                -- x, y :: Either String Int
+    x   <- dbLookup1                      -- x :: IO (Either String Int)
+    y   <- dbLookup2                      -- y :: IO (Either String Int)
+    return $ addEitherValues x y          -- x, y :: Either String Int
 
 -- needed to add Either values
 addEitherValues :: Either String Int -> Either String Int -> Either String Int
 addEitherValues (Left x) _ = Left x
 addEitherValues _ (Left x) = Left x
 addEitherValues (Right x) (Right y) = Right (x + y)
+
+
+-- using monad transformers: ExceptT
+-- newtype ExceptT e m a
+-- ExceptT (m (Either e a))
+type DatabaseIO = ExceptT String IO
+dbLookup1' :: DatabaseIO Int            -- useful to think: (ExceptT String IO) = m => m Int
+dbLookup1' = return 7
+
+dbLookup2' :: DatabaseIO Int
+dbLookup2' = return 8
+
+dbProgram'' = do
+    putStrLn "This program is useless but nice."
+    runExceptT $ do        -- unwrap IO () from ExceptT
+        x   <- dbLookup1'         -- x :: Int
+        y   <- dbLookup2'         -- y :: Int
+        return ( x + y )
+
+
+
+
 
 
